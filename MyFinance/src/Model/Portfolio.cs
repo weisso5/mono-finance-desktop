88d6523968235d6c3ce46b5395e8e85ae745d4ab
mweissman@weisso.com <mweissman@weisso.com@292ea6d8-64eb-96ff-cad5-981d7c9daa9d>
2011-04-25 23:17:31 +0000

    small progress, still working on Getting and Parsing securities

--- MyFinance/src/Model/Portfolio.cs
+++ MyFinance/src/Model/Portfolio.cs
@@ -1,19 +1,22 @@
 using System;
 using System.Xml;
+using System.Xml.XPath;
 
 namespace MyFinance
 {
 	/// <summary>
 	/// Portfolio Entity
 	/// </summary>
-	public class Portfolio
+	public  sealed class Portfolio
 	{
+		private static readonly log4net.ILog log = log4net.LogManager.GetLogger(typeof(MainWindow));
 		
-		private string ID {get;set;}
-		private DateTime Updated {get;set;}
-		private DateTime Edited {get;set;}
-		private string Title {get;set;}
-		private string FeedLink {get;set;}
+		public string ID {get;set;}
+		public string eTag {get;set;}
+		public DateTime Updated {get;set;}
+		public DateTime Edited {get;set;}
+		public string Title {get;set;}
+		public string FeedLink {get;set;}
 		
 		//TODO Add Portfolio Data
 		
@@ -21,17 +24,35 @@ namespace MyFinance
 		{
 		}
 		
-		public Portfolio(XmlNode portfolioNode){
-			ParseNode(portfolioNode);	
+		public Portfolio(string portfolioNodeOuterXml){
+			ParseNode(portfolioNodeOuterXml);	
 		}
 		
-		private void ParseNode(XmlNode node)
+		private void ParseNode(string outerXML)
 		{
-			this.ID = node.SelectNodes("id").Item(0).InnerText;
-			this.Updated = DateTime.Parse(node.SelectNodes("updated").Item(0).InnerText);
-			this.Edited  = DateTime.Parse(node.SelectNodes("edited").Item(0).InnerText);
-			this.Title = node.SelectNodes("title").Item(0).InnerText;
-			this.FeedLink = node.SelectNodes("feedLink").Item(0).Attributes["href"].Value;
+			XmlDocument node = new XmlDocument();
+			node.LoadXml(outerXML);
+			
+			if(!node.HasChildNodes)
+			{
+				return; //skip childless
+			}
+			
+			try {				
+				this.ID = node.GetElementsByTagName("id").Item(0).InnerText.Replace("http://","").Split('/')[5];
+				this.eTag = node.GetElementsByTagName("entry").Item(0).Attributes.GetNamedItem("etag").Value; // save this for If-none-Match
+				this.Updated = DateTime.Parse(node.GetElementsByTagName("updated").Item(0).InnerText);
+				this.Edited  = DateTime.Parse(node.GetElementsByTagName("edited").Item(0).InnerText);
+				this.Title = node.GetElementsByTagName("title").Item(0).InnerText;
+				this.FeedLink = node.GetElementsByTagName("feedlink").Item(0).Attributes["href"].Value;
+			} catch (XmlException ex) {
+				log.Warn(ex.Message);
+			} catch (XPathException ex1) {
+				log.Warn(ex1.Message);
+			} catch (NullReferenceException ex2)
+			{
+				log.Warn(ex2.Message);
+			}
 		}
 	}
 }
